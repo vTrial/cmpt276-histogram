052e7925af847d1c057f1eef9f176c10f1805736
Sean Lam <orange75100@gmail.com>
2023-05-28 18:20:08 -0700

    error handling sucks

--- src/main/resources/static/calculate.js
+++ src/main/resources/static/calculate.js
@@ -1,19 +1,20 @@
 const addToHist = (gradeIn) => {
-    let lowerBounds = document.querySelectorAll('.lower-bounds-input');
-    let lowerBoundFound = false;
-    let lowerBoundsIndex = 0;
-    while (!lowerBoundFound && lowerBoundsIndex < lowerBounds.length) {
-        if (gradeIn > Number(lowerBounds[lowerBoundsIndex].value)) {
-            lowerBoundsIndex -= 1;
-            lowerBoundFound = true;
-        }
-        lowerBoundsIndex += 1;
+    let lowerBoundsTemp = document.querySelectorAll('.lower-bounds-input');
+    let lowerBoundsTempFound = false;
+    let lowerBoundsTempIndex = 0;
+    if (gradeIn < 0) {
+        alert("The grade inputted is too low");
+        return;
+    } else if (gradeIn > 100) {
+        alert("The grade inputted is too high");
+        return;
     }
-    // if lower than f, we don't crash :)
-    if (!lowerBoundFound) {
-        alert("There's a number which is below all thresholds.")
-        const index = grades.indexOf(gradeIn);
-        grades.splice(index, 1)
+    while (!lowerBoundsTempFound && lowerBoundsTempIndex < lowerBoundsTemp.length) {
+        if (gradeIn > Number(lowerBoundsTemp[lowerBoundsTempIndex].value)) {
+            lowerBoundsTempIndex -= 1;
+            lowerBoundsTempFound = true;
+        }
+        lowerBoundsTempIndex += 1;
     }
     let lowerBoundsToHist = document.querySelectorAll(".histogram-value")[lowerBoundsIndex];
     lowerBoundsToHist.innerHTML += "✗";
@@ -25,7 +26,47 @@ const clearHist = () => {
         hist.innerHTML = ""
     });
 }
+
+const checkBounds = () => {
+    let lowerBoundsTemp = document.querySelectorAll('.lower-bounds-input');
+    for (let i = 0; i < lowerBoundsTemp.length; i++) {
+        let boundsInt = Number(lowerBoundsTemp[i].value)
+        if (boundsInt > 100) {
+            lowerBoundsTemp[i].value = 100
+            boundsInt = 100
+        }
+        else if (boundsInt < 0) {
+            lowerBoundsTemp[i].value = 0
+            boundsInt = 0
+        }
+        if (i > 0) {
+            let boundsPrev = Number(lowerBoundsTemp[i - 1].value)
+            if (boundsInt > boundsPrev && boundsInt > gradesBounds[i]) {
+                for (let j = 1; j < i; j++) {
+                    lowerBoundsTemp[j].value = boundsInt
+                }
+            }
+        }
+        if (i < lowerBoundsTemp.length - 1) {
+            let boundsNext = Number(lowerBoundsTemp[i + 1].value)
+            if (boundsInt < boundsNext && boundsInt < gradesBounds[i]) {
+                for (let j = lowerBoundsTemp.length - 2; j > i; j--) {
+                    lowerBoundsTemp[j].value = boundsInt
+                }
+            }
+        }
+    }
+    gradesBounds = []
+    lowerBoundsTemp.forEach(bound => {
+        gradesBounds.push(Number(bound.value))
+    })
+}
 let grades = []
+let gradesBoundsEles = document.querySelectorAll('.lower-bounds-input')
+gradesBounds = []
+gradesBoundsEles.forEach(grade => {
+    gradesBounds.push(grade.value);
+  });
 
 let newGrade = document.getElementById("new-grade")
 newGrade.onkeydown = (event) => {
@@ -37,6 +78,7 @@ newGrade.onkeydown = (event) => {
 };
 let lowerBounds = document.getElementById("lower-bounds")
 lowerBounds.onchange = () => {
+    checkBounds()
     clearHist()
     grades.forEach(grade => {
         addToHist(grade);
